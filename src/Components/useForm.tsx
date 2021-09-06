3dec28c27142f4fb57f4b4d336192b9d2cb153d2
Zoltán <tothz25@gmail.com>
2021-09-06 18:42:57 +0200

    changed email validation pattern

--- src/Components/useForm.tsx
+++ src/Components/useForm.tsx
@@ -34,8 +34,7 @@ export const validate = (data: Data) => {
     errors.email = "This field is required";
   }
   if (data.email) {
-    const pattern =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const pattern = /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/;
     if (!pattern.test(data.email)) {
       errors.email = "Please use valid email";
     }
